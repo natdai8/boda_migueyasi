30d21835f534b069bf3479bcd79192a0fdabbf60
Fran M <fr.me.jim@gmail.com>
2022-11-20 19:37:30 +0100

    added some fixes

--- js/main.js
+++ js/main.js
@@ -5,18 +5,23 @@ let isToggled = false;
 
 const handleNavbarToggle = () => {
     const collapse = document.getElementById("nav-collapse");
+    const navbar = document.getElementById("navbar-wrapper");
     const button = document.getElementById("toggle-collapse-btn");
 
     if (isToggled) {
         isToggled = false;
         button.className = button.className.replace(" toggle-on", "");
+        navbar.className = navbar.className.replace(" navbar-solid", "");
+        collapse.className = collapse.className.replace(" navbar-solid", "");
         collapse.className += " collapse-action";
         return;
     }
 
     isToggled = true;
     button.className += " toggle-on";
+    navbar.className += " navbar-solid";
     collapse.className = collapse.className.replace(" collapse-action", "");
+    collapse.className += " navbar-solid";
 };
 
 window.addEventListener("load", () => {
